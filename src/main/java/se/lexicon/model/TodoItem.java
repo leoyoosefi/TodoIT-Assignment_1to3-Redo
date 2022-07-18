25b95cd2b40d9269d0bd05e40bd6856345612ed3
Leo Yoosefi <leoyoosefi@gmail.com>
2022-07-18 15:46:08 +0200

    other classes created

--- src/main/java/se/lexicon/model/TodoItem.java
+++ src/main/java/se/lexicon/model/TodoItem.java
@@ -1,12 +1,105 @@
 package se.lexicon.model;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class TodoItem {
 
     private int id;
     private String title;
     private String taskDescription;
-    boolean done;
-    Person creator;
+    LocalDate deadLine;
+    private boolean done;
+    private Person creator;
 
+    public TodoItem(int id, String title, String taskDescription, LocalDate deadLine, boolean done, Person creator) {
+        this(title,taskDescription,deadLine,done,creator);
+        this.id = id;
+    }
 
+    public TodoItem(String title, String taskDescription, LocalDate deadLine, boolean done, Person creator) {
+        setTitle(title);
+        setTaskDescription(taskDescription);
+        setDeadLine(deadLine);
+        setDone(done);
+        setCreator(creator);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if(title == null) throw new IllegalArgumentException("title is null");
+        this.title = title;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public LocalDate getDeadLine() {
+        return deadLine;
+    }
+
+    public void setDeadLine(LocalDate deadLine) {
+        this.deadLine = deadLine;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Person getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Person creator) {
+        this.creator = creator;
+    }
+
+    public boolean overdue(){
+        return LocalDate.now().isAfter(deadLine);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TodoItem todoItem = (TodoItem) o;
+        return id == todoItem.id && done == todoItem.done && Objects.equals(title, todoItem.title) && Objects.equals(taskDescription, todoItem.taskDescription) && Objects.equals(deadLine, todoItem.deadLine) && Objects.equals(creator, todoItem.creator);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, taskDescription, deadLine, done, creator);
+    }
+
+    @Override
+    public String toString() {
+        return "TodoItem{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", taskDescription='" + taskDescription + '\'' +
+                ", deadLine=" + deadLine +
+                ", done=" + done +
+                ", creator=" + creator +
+                '}';
+    }
 }
